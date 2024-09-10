ff5f9e4da977657519d0deddbca5c7a2db743b25
VKubrin <v.r.kubrin.58@mail.ru>
2024-09-10 23:19:12 +0300

    Add files via upload
    Строка с удалением пробела даёт ошибку. строка для ввода "Владимир 66 лет"

---
+++ kvr_module_1_4.py
@@ -0,0 +1,32 @@
+my_string = input('введите ваше имя и возраст: ')
+count = len(my_string)
+print(count)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
